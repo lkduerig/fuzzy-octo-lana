002a42d3a8eecae977584ba8956cbb29cdcaa689
Liisa <lkduerig@LisMac2.local>
2012-10-13 12:23:40 +0300

    material ordering

---
+++ materialordering/materialordering.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function() {
+	jQuery('#edit-contact input:text').addClass('default');
+	
+  jQuery("#edit-contact input:text").focus(function() {
+		this.value = this.value != this.defaultValue ? this.value : '';
+  });
+  jQuery("#edit-contact input:text").blur(function() {
+		this.value = this.value.trim().length > 0 ? this.value : this.defaultValue;
+		jQuery(this).toggleClass('changed', this.value != this.defaultValue).toggleClass('default', this.value == this.defaultValue);
+  });
+});
